ebccf3e357f4bd1579682c8694e45a6f7f51c182
GrzegorzDabrowski <70518773+GrzegorzDabrowski@users.noreply.github.com>
2021-02-01 12:32:41 +0100

    login and register sections done

--- portoflio-lab/src/components/pages/Login.js
+++ portoflio-lab/src/components/pages/Login.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import HomeHeader from "./Home/HomeHeader";
+import decoration from "../../assets/Decoration.svg";
+import { useFormik } from "formik";
+import Home from "./Home/Home";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Tu się zalogujesz</h1>;
+  const [errorLogin, setErrorLogin] = useState("");
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email) &&
+      values.email.length
+    ) {
+      errors.email = "Podany email jest nieprawidłowy!";
+      setErrorLogin("");
+    }
+
+    if (values.password.length < 6 && values.password.length) {
+      errors.password = "Hasło jest za krótkie!";
+      setErrorLogin("");
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (e) => {},
+  });
+
+  return (
+    <section className="login-page">
+      <HomeHeader />
+      <div className="login-page-content">
+        <h1>Zaloguj się</h1>
+        <img src={decoration} alt="" />
+        <form>
+          <div className="form-inputs">
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email" />
+            <label htmlFor="password">Hasło</label>
+            <input id="password" name="password" type="password" />
+          </div>
+        </form>
+        <div className="form-buttons">
+          <button type="submit">Zaloguj się</button>
+          <Link className="register" to="/rejestracja">
+            <h2>Załóż konto</h2>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- portoflio-lab/src/components/pages/Register.js
+++ portoflio-lab/src/components/pages/Register.js
@@ -1,7 +1,55 @@
 import React from "react";
+import HomeHeader from "./Home/HomeHeader";
+import decoration from "../../assets/Decoration.svg";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <h1>Tu się zarejestrujesz</h1>;
+  const validate = (values) => {
+    const errors = {};
+
+    if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email) &&
+      values.email.length
+    ) {
+      errors.email = "Podany email jest nieprawidłowy!";
+    }
+
+    if (values.password.length < 6 && values.password.length) {
+      errors.password = "Hasło jest za krótkie!";
+    }
+
+    if (values.password !== values.passwordRepeat) {
+      errors.passwordRepeat = "Hasła nie są takie same!";
+    }
+
+    return errors;
+  };
+
+  return (
+    <section className="register-page">
+      <HomeHeader />
+      <div className="register-page-content">
+        <h1>Załóż konto</h1>
+        <img src={decoration} alt="" />
+        <form>
+          <div className="register-form-inputs">
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email" />
+            <label htmlFor="password">Hasło</label>
+            <input id="password" name="password" type="password" />
+            <label htmlFor="password">Powtórz hasło</label>
+            <input id="password" name="password" type="password" />
+          </div>
+        </form>
+        <div className="register-form-buttons">
+          <button type="submit">Załóż konto</button>
+          <Link className="login" to="/logowanie">
+            <h2>Zaloguj się</h2>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
